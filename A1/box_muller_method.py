7ca44e64fbf78532b3425773c248eeb6d96dbf60
vHanzel <vid.hanzel@gmail.com>
2025-04-04 19:08:42 +0200

    Implemented the marsagila bray scheme and added the visualization code

--- A1/box_muller_method.py
+++ A1/box_muller_method.py
@@ -32,7 +32,25 @@ def marsaglia_bray_scheme(sample_size: int) -> np.ndarray:
     :param sample_size: size of random sample to be generated
     :return: 2d numpy array
     """
-    pass
+    # only roguhly pi/4 are within the unit circle, so we need to generate more points than sample_size , we generate sample_size/ (pi/4) points plus 5% margin
+    N = int((sample_size / (np.pi / 4))*1.05)
+
+    
+    u_hat = 2*np.random.uniform(0, 1, N)-1
+    v_hat = 2*np.random.uniform(0, 1, N)-1
+
+    r = u_hat**2 + v_hat**2 
+    mask = r < 1 # only keep points within the unit circle
+
+
+    x = u_hat[mask] * np.sqrt(-2 * np.log(r[mask]) / r[mask])
+    y = v_hat[mask] * np.sqrt(-2 * np.log(r[mask]) / r[mask])
+    generated_samples = np.array([x, y]) 
+    # if we have more samples than needed, we take only the first sample_size samples
+    generated_samples = generated_samples[:, :sample_size]
+
+    return generated_samples  # shape (2, sample_size)
+
 
 def affine_linear_transformation(x: np.ndarray, a: np.ndarray, b: np.ndarray) -> np.ndarray:
     """
@@ -44,6 +62,7 @@ def affine_linear_transformation(x: np.ndarray, a: np.ndarray, b: np.ndarray) ->
     :param b: 2d numpy array of shape (2, 1) representing the translation vector
     :return: 2d numpy array of shape (2. n)
     """
+    
     return np.matmul(a, x) + b
 
 def marginal_max_likelihood_estimators(x: np.ndarray) -> Tuple[float, float]:
@@ -69,33 +88,57 @@ def visualise(sample_array_dict: Dict[str, np.ndarray]):
     :return:
     """
 
-    
-    pass
+    fig, ax = plt.subplots(1,3 , figsize=(15, 5))
+    for i, (k, samples) in enumerate(sample_array_dict.items()):
+        
+        ax[i].scatter(samples[0], samples[1])
+        ax[i].set_title(f"Samples from {k}")
+        ax[i].set_xlabel('X-axis')
+        ax[i].set_ylabel('Y-axis')
+        ax[i].set_ylim(-7, 7)
+        ax[i].set_xlim(-7, 7)
+    plt.tight_layout()
+    plt.savefig('sample_plots.png')
+    plt.show()
 
 def main():
-    sample_size = 'fill me'
+    sample_size = 100000  # number of samples to be generated
     box_muller_sample = box_muller_scheme(sample_size)
     marsaglia_bray_sample = marsaglia_bray_scheme(sample_size)
+    # linear transformation for multivariate normal distribution = a * x + b
+    mu1 = np.array([[-1, 1]])
+    mu2 = np.array([[0, 0]])
+    sig1 = np.array([[1/100, 0], [0, 1]])
+    sig2 = np.array([[5, 6], [6, 9]])
 
-    a1 = 'fill me'
-    b1 = 'fill me'
-    a2 = 'fill me'
-    b2 = 'fill me'
+
+    
+
+    a1 = np.sqrt(sig1)  # we dont need cholsky as is its already diagonal we can just take the square root
+    b1 = mu1.T
+    a2 = np.linalg.cholesky(sig2)  # Cholesky decomposition of covariance matrix
+    b2 = mu2.T
 
     # transformations of box-muller sample
     transformed_sample_1 = affine_linear_transformation(box_muller_sample, a1, b1)
     transformed_sample_2 = affine_linear_transformation(box_muller_sample, a2, b2)
 
+    
+
+    
     # transformations of marsaglia-bray sample
     transformed_sample_3 = affine_linear_transformation(marsaglia_bray_sample, a1, b1)
     transformed_sample_4 = affine_linear_transformation(marsaglia_bray_sample, a2, b2)
 
     # maximum likelihood estimators of expected value and variance of the marginals
-    for sample in [transformed_sample_1, transformed_sample_2,
-                   transformed_sample_3, transformed_sample_4]:
-        mu_hat_1, sig_hat_sq_1 = marginal_max_likelihood_estimators('fill me')
-        mu_hat_2, sig_hat_sq_2 = marginal_max_likelihood_estimators('fill me')
-
+    for i, sample in enumerate([transformed_sample_1, transformed_sample_2, transformed_sample_3, transformed_sample_4]):
+        mu_hat_1, sig_hat_sq_1 = marginal_max_likelihood_estimators(sample[0])
+        mu_hat_2, sig_hat_sq_2 = marginal_max_likelihood_estimators(sample[1])
+        if i == 0:
+            print('########################box-muller scheme##########################')
+        
+        if i == 2:
+            print('########################marsaglia-bray scheme##########################')
         print('likelihood estimators of marginal expected value')
         print(mu_hat_1)
         print(mu_hat_2)
